d8218d5b9234d915f5dabf3cb9d90bc9ae41c1bf
shahinataba-droid <shahin.ataba@gmail.com>
2026-09-23 07:36:11 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 // Service worker: gör appen installerbar och användbar utan nätverk.
 // CACHE-namnet innehåller en kontrollsumma av app.js, vilket är signalen
 // webbläsaren behöver för att hämta en ny version i stället för den cachade.
-const CACHE = "min-ekonomi-34560fdc49";
-const ASSETS = ["./", "./index.html", "./app.js?v=34560fdc49", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
+const CACHE = "min-ekonomi-3e8e9dbf62";
+const ASSETS = ["./", "./index.html", "./app.js?v=3e8e9dbf62", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
